c4066fd1224a0e673045141e14cf676ef73edb1d
Oxayr <uzairmaqsood451@gmail.com>
2024-11-14 13:21:41 +0500

    added the cleasses for flat list

--- App.js
+++ App.js
@@ -7,30 +7,53 @@ import {
   Text,
   Image,
   TouchableOpacity,
+  FlatList,
 } from 'react-native';
 import backgroundImage from './src/assets/Bg.png';
 import {
   heightPercentageToDP,
   widthPercentageToDP,
 } from 'react-native-responsive-screen';
+import ChatListItem from './src/components/ChatListItem';
 
 const App = () => {
+  const DATA = [
+    {
+      message:
+        'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba sad asd as dasd asd as da sad',
+      name: 'First Item',
+    },
+    {
+      message: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+      name: 'Second Item',
+    },
+    {
+      message: '58694a0f-3da1-471f-bd96-145571e29d72',
+      name: 'Third Item',
+    },
+  ];
   return (
     <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
-      <ImageBackground
-        source={backgroundImage}
-        resizeMode="cover"
-        style={styles.image}>
-        <View style={styles.headerContainer}>
-          <View style={styles.backButton}>
-            <Text style={styles.buttonText}>View 1</Text>
+      <ImageBackground source={backgroundImage} resizeMode="cover">
+        <View style={[styles.imageOverlay, styles.image]}>
+          <View style={styles.headerContainer}>
+            <View style={styles.backButton}>
+              <Text style={styles.buttonText}>{`<`}</Text>
+            </View>
+            <View style={styles.headerButtonContainer}>
+              <TouchableOpacity style={styles.headerButton}>
+                <Text style={styles.buttonText}>Send to all</Text>
+              </TouchableOpacity>
+            </View>
+            <Image style={styles.profile} source={backgroundImage} />
           </View>
-          <View style={styles.headerButtonContainer}>
-            <TouchableOpacity style={styles.headerButton}>
-              <Text style={styles.buttonText}>Send to all</Text>
-            </TouchableOpacity>
+          <View>
+            <FlatList
+              data={DATA}
+              renderItem={({item}) => <ChatListItem item={item} />}
+              keyExtractor={item => item.name}
+            />
           </View>
-          <Image style={styles.profile} source={backgroundImage} />
         </View>
       </ImageBackground>
     </SafeAreaView>
@@ -42,13 +65,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   backButton: {
-    width: widthPercentageToDP(20),
-    height: heightPercentageToDP(8),
-    backgroundColor: 'red',
-
+    width: widthPercentageToDP(13),
+    height: heightPercentageToDP(6),
+    backgroundColor: '#3263B8',
+    borderColor: '#508AFE',
+    borderRadius: widthPercentageToDP(8.5),
+    borderRightWidth: widthPercentageToDP(0.5),
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   headerButtonContainer: {
     width: widthPercentageToDP(50),
     height: heightPercentageToDP(8),
@@ -66,22 +92,24 @@ const styles = StyleSheet.create({
     width: widthPercentageToDP(13),
     height: heightPercentageToDP(6),
     borderRadius: widthPercentageToDP(8.5),
-    backgroundColor: 'green',
   },
   headerContainer: {
     marginVertical: heightPercentageToDP(2),
     flexDirection: 'row',
     justifyContent: 'space-evenly',
     alignItems: 'center',
-    gap: widthPercentageToDP(2),
+    gap: widthPercentageToDP(5),
   },
   image: {
     flex: 1,
     alignItems: 'center',
   },
+  imageOverlay: {
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
   buttonText: {
     color: 'white',
-    fontSize: 16,
+    fontSize: widthPercentageToDP(4),
     fontWeight: 'bold',
   },
 });
---
+++ src/components/ChatListItem.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {View, Text, StyleSheet, ImageBackground, Image} from 'react-native';
+import backgroundImage from '../assets/blur-Bg.png';
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from 'react-native-responsive-screen';
+
+const ChatListItem = ({item}) => {
+  return (
+    <ImageBackground
+      source={backgroundImage}
+      style={styles.image}
+      imageStyle={styles.imageBorderRadius}>
+      <View style={styles.container}>
+        <Image source={backgroundImage} style={styles.profile} />
+
+        <View style={styles.userInfo}>
+          <Text style={styles.text}>{item?.name}</Text>
+          <Text numberOfLines={1} style={styles.text}>
+            {item?.message}
+          </Text>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: widthPercentageToDP(1),
+  },
+  imageBorderRadius: {
+    borderRadius: widthPercentageToDP(3),
+    borderWidth: widthPercentageToDP(0.1),
+    borderColor: 'gray',
+  },
+  container: {
+    flexDirection: 'row',
+    width: widthPercentageToDP(85),
+    gap: widthPercentageToDP(3),
+    height: heightPercentageToDP(7),
+    marginVertical: heightPercentageToDP(1),
+    paddingHorizontal: widthPercentageToDP(4),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  profile: {
+    width: widthPercentageToDP(12),
+    height: heightPercentageToDP(6),
+    borderRadius: widthPercentageToDP(8.5),
+  },
+  userInfo: {
+    flex: 1,
+    gap: heightPercentageToDP(1),
+  },
+  text: {
+    color: 'white',
+  },
+});
+
+export default ChatListItem;
